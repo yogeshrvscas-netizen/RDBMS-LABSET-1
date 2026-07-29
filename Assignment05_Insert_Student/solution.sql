ecb77b128c8885e267ff039ed1be935981351bc8
yogeshrvscas-netizen <yogeshrvscas@gmail.com>
2026-07-29 10:35:14 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -21,3 +21,16 @@ CREATE TABLE Student(
 -- 1003 Karthik
 
 -- Display all records
+
+CREATE TABLE Student(
+studentID INT PRIMARY KEY,
+studentName VARCHAR(50),
+Gender VARCHAR(10),
+DepartmentID INT);
+
+INSERT INTO Student (StudentID, StudentName, Gender, DeaprtmentID)
+VALUES (1001, 'Arun', 'Male', 101),
+(1002, 'Divya', 'Female', 102),
+(1003, 'Karthick', 'Male', 101);
+
+SELECT*FROM Student
